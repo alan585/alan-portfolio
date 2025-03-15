25681a89d1ce254c9959a7a674ade7f0a219d7dc
Alan Alvarado <alanalvarado585@gmail.com>
2025-03-14 21:57:10 -0500

    feat(about section): populated skill cards

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -21,8 +21,8 @@ const CursorGradientCircle = () => {
     <div
       className="pointer-events-none fixed left-0 top-0 h-screen w-screen opacity-30 max-lg:hidden"
       style={{
-        background: `radial-gradient(circle at ${mousePosition.x}px ${mousePosition.y}px, rgba(30, 58, 138, 0.7), rgba(30, 58, 138, 0) 800px)`,
-        transition: "background 0.3s ease-out",
+        background: `radial-gradient(circle at ${mousePosition.x}px ${mousePosition.y}px, rgba(30, 58, 138, 0.7), rgba(30, 58, 138, 0) 1000px)`,
+        transition: "background 0.1s ease-out",
       }}
     ></div>
   );
--- src/constants/index.js
+++ src/constants/index.js
@@ -4,8 +4,12 @@ import {
   FaCss3,
   FaJs,
   FaReact,
-  FaFigma,
   FaNodeJs,
+  FaPython,
+  FaDatabase,
+  FaJava,
+  FaLinux,
+  FaShopify,
 } from "react-icons/fa";
 
 import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
@@ -132,8 +136,24 @@ const skills = {
       name: "node.js",
     },
     {
-      icon: <FaFigma />,
-      name: "figma",
+      icon: <FaPython />,
+      name: "python",
+    },
+    {
+      icon: <FaDatabase />,
+      name: "SQL",
+    },
+    {
+      icon: <FaJava />,
+      name: "java",
+    },
+    {
+      icon: <FaLinux />,
+      name: "linux",
+    },
+    {
+      icon: <FaShopify />,
+      name: "shopify",
     },
   ],
 };
@@ -204,4 +224,49 @@ const services = [
   },
 ];
 
-export { about, experience, education, skills, socialLinks, stats, services };
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "project 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "02",
+    category: "fullstack",
+    title: "project 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
+    stack: [{ name: "Next.js" }, { name: "Tailwind.css" }, { name: "Node.js" }],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "frontend",
+    title: "project 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
+    stack: [{ name: "Next.js" }, { name: "Tailwind.css" }],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: "",
+  },
+];
+
+export {
+  about,
+  experience,
+  education,
+  skills,
+  socialLinks,
+  stats,
+  services,
+  projects,
+};
