1f0c84c7bb374fb54a811f0e6a674951218732a0
Alan Alvarado <alanalvarado585@gmail.com>
2025-03-18 15:49:38 -0500

    feat(Work Section): fix mobile page structure

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -32,8 +32,8 @@ const Work = () => {
       className="flex min-h-[80vh] flex-col"
     >
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row">
-          <div className="flex flex-col gap-6">
+        <div className="flex flex-col py-12 xl:flex-row xl:gap-12 xl:pt-40">
+          <div className="order-1 flex flex-col gap-6 xl:order-none">
             <h1 className="text-outline text-8xl font-extrabold text-transparent">
               {project.num}
             </h1>
